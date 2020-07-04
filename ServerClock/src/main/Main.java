d80bfc964f896118e56047f83cf6f667246165d9
asincov <sashasintsov@mail.ru>
2020-07-04 09:40:13 +0300

    b35

--- ServerClock/src/main/Main.java
+++ ServerClock/src/main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Server Clock");
+        primaryStage.setTitle("Aphorism server");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
